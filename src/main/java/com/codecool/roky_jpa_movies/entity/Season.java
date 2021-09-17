302f162dfa6af20eb40894012a7585a6eab42f39
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-18 00:07:58 +0200

    [add] basic Lombok annotations to Episode/Season classes

--- src/main/java/com/codecool/roky_jpa_movies/entity/Season.java
+++ src/main/java/com/codecool/roky_jpa_movies/entity/Season.java
@@ -1,4 +1,24 @@
 package com.codecool.roky_jpa_movies.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Builder
+@Entity
 public class Season {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
 }
