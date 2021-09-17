bb894d035636a00084bbebfcc04a8db507d33376
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-17 20:07:59 +0200

    [init] bootstrap Spring Boot with Tests, Lombok & H2

---
+++ src/main/java/com/codecool/roky_jpa_movies/RokyJpaMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.roky_jpa_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RokyJpaMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RokyJpaMoviesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/codecool/roky_jpa_movies/entity/Genre.java
@@ -0,0 +1,8 @@
+package com.codecool.roky_jpa_movies.entity;
+
+public enum Genre {
+
+    CRIME,
+    DRAMA,
+    THRILLER
+}
