aadc736859520cdf6264577e45ad92703900db03
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-18 00:08:36 +0200

    [add] Series fields with annotations and constraints

--- src/main/java/com/codecool/roky_jpa_movies/entity/Series.java
+++ src/main/java/com/codecool/roky_jpa_movies/entity/Series.java
@@ -2,25 +2,60 @@ package com.codecool.roky_jpa_movies.entity;
 
 import lombok.*;
 
-import javax.persistence.ElementCollection;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.List;
 
-@Entity
+import static javax.persistence.GenerationType.*;
+
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
+@ToString
+@Builder
+@Entity(name = "Series")
+@Table(
+        name = "series",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "student_title_unique", columnNames = "title")
+        }
+)
 public class Series {
 
     @Id
-    @GeneratedValue
+    @SequenceGenerator(
+            name = "series_sequence",
+            sequenceName = "series_sequence",
+            allocationSize = 1,
+            initialValue = 100
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "series_sequence"
+    )
+    @Column(
+            name="id",
+            updatable = false
+    )
     private Long id;
 
+    @Column(
+            name = "title",
+            nullable = false,
+            unique = true
+    )
     private String title;
+
+    @Column(name="description")
     private String description;
+
+    @Column(
+            name="first_air_date",
+            nullable = false
+    )
+    private LocalDate airDate;
 
     @ElementCollection
     @Singular
